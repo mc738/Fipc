29a961e744dd8e678fd3f08761b489ad183ce2e2
Max Clifford <me@mclifford.dev>
2022-01-10 00:10:09 +0000

    Cleaned up examples.

--- Fipc.ExampleClient/Program.fs
+++ Fipc.ExampleClient/Program.fs
@@ -5,28 +5,41 @@ open System.Text
 open Fipc.Core
 open Fipc.Core.Common
 
+
+[<RequireQualifiedAccess>]
+module Examples =
+
+    let hookClient _ =
+        let config =
+            ({ Id = "example-client"
+               ChannelType = FipcChannelType.NamedPipe "testpipe"
+               MaxThreads = 1
+               ContentType = FipcContentType.Text
+               EncryptionType = FipcEncryptionType.None
+               CompressionType = FipcCompressionType.None
+               Key = Encoding.UTF8.GetBytes "Hello, World!" }: FipcConnectionConfiguration)
+
+        let writer = Client.startHookClient config
+
+        let rec testLoop () =
+            printfn "Enter message to send to server"
+            printf "> "
+            let message = Console.ReadLine()
+            match writer.TryPostMessage(FipcMessage.StringMessage($"Time: {DateTime.Now} Message: {message}")) with
+            | Ok _ -> ()
+            | Error e -> printfn $"{e}"
+            testLoop ()
+
+        printfn $"Starting client loop."
+        testLoop ()
+
+
+
+
+
 [<EntryPoint>]
 let main argv =
+
     
-    let config = ({
-        Id = "example-client"
-        ChannelType = FipcChannelType.NamedPipe "testpipe"
-        MaxThreads = 1
-        ContentType = FipcContentType.Text
-        EncryptionType = FipcEncryptionType.None
-        CompressionType = FipcCompressionType.None
-        Key = Encoding.UTF8.GetBytes "Hello, World!"
-    }: FipcConnectionConfiguration)
-    
-    let writer = Client.startHookClient config
-    
-    let rec testLoop () =
-        match writer.TryPostMessage(FipcMessage.StringMessage($"Time now is {DateTime.Now}.")) with
-        | Ok _ -> ()
-        | Error e -> printfn $"{e}"
-        Async.Sleep 1000 |> Async.RunSynchronously
-        testLoop ()
-        
-    printfn $"Starting client loop."
-    testLoop ()
+    Examples.hookClient ()
     0 // return an integer exit code
--- Fipc.ExampleServer/Program.fs
+++ Fipc.ExampleServer/Program.fs
@@ -4,32 +4,48 @@ open System.Text
 open Fipc.Core
 open Fipc.Core.Common
 
-[<EntryPoint>]
-let main argv =
-
-    let config = ({
-        Id = "example-server"
-        ChannelType = FipcChannelType.NamedPipe "testpipe"
-        MaxThreads = 1
-        ContentType = FipcContentType.Text
-        EncryptionType = FipcEncryptionType.None
-        CompressionType = FipcCompressionType.None
-        Key = Encoding.UTF8.GetBytes "Hello, World!"
-    }: FipcConnectionConfiguration)
-    
-    
-    let reader = Server.startHookServer config 
-    
+/// An example listener, this will simply print the messages it receives.
+let listener (reader: FipcConnectionReader) =
     let rec testLoop () =
         match reader.TryReadMessage() with
         | Some msg ->
             match msg.Body with
             | FipcMessageContent.Text t -> printfn $"Message: {t}"
             | _ -> printfn $"Message type not supported yet."
-        | None -> printfn $"No messages."
+        | None -> () //printfn $"No messages."
+
         Async.Sleep 1000 |> Async.RunSynchronously
         testLoop ()
-    
-    printfn $"Starting server loop."
-    testLoop ()    
+
+    printfn $"Starting example server loop."
+    testLoop ()
+
+[<RequireQualifiedAccess>]
+module Examples =
+
+    /// Example of how to ingrate a hook server.
+    let hookServer _ =
+        
+        // 1. An example configuration.    
+        let config =
+            ({ Id = "example-server"
+               ChannelType = FipcChannelType.NamedPipe "testpipe"
+               MaxThreads = 1
+               ContentType = FipcContentType.Text
+               EncryptionType = FipcEncryptionType.None
+               CompressionType = FipcCompressionType.None
+               Key = Encoding.UTF8.GetBytes "Hello, World!" }: FipcConnectionConfiguration)
+            
+        
+        // 2. Start the server. This returns a FipcConnectionReader. 
+        let reader = Server.startHookServer config
+        
+        // 3. Use the reader. In this example a loop is run that listens for messages and prints them.
+        listener reader
+        
+
+[<EntryPoint>]
+let main argv =
+
+    Examples.hookServer ()
     0 // return an integer exit code

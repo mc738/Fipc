7642138c92f9c95373633506ffec46f1802786e0
Max Clifford <me@mclifford.dev>
2022-01-10 00:12:18 +0000

    Fixed tests and removed Library.fs

--- Fipc.UnitTests/Common.fs
+++ Fipc.UnitTests/Common.fs
@@ -26,10 +26,10 @@ module Common =
             let expected = FipcMessage.StringMessage("Hello, World!")
             
             use ms = new MemoryStream()
-            Operations.tryWriteMessage "test-write"  ms expected
+            Operations.tryWriteMessage ms expected
             |> Result.bind (fun _ ->
                 ms.Seek(0, SeekOrigin.Begin) |> ignore
-                Operations.tryReadMessage "test-read" FipcContentType.Text ms)
+                Operations.tryReadMessage FipcContentType.Text ms)
             |> fun r ->
                 match r with
                 | Ok actual -> Assert.AreEqual(expected, actual)
